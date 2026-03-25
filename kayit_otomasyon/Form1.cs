0e9a9df1a37170d8996a62a7f456db0cc0ab60d2
mefadanist <mefadanist@gmail.com>
2026-03-25 11:52:30 +0300

    fe

--- kayit_otomasyon/Form1.cs
+++ kayit_otomasyon/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace kayit_otomasyon
 {
     public partial class Form1 : Form
     {
+
+
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
